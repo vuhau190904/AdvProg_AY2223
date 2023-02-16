1a718de5a7b4aac057e6980960bb24a977e8846c
vuhau190904 <vuhau190904@gmail.com>
2023-02-16 15:04:25 +0700

    update

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -106,7 +106,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
     // TODO: Update the secret word if the character ch is in the answer word.
     for(int i = 0; i < int(word.size()); ++i)
     {
-        if(i == ch)
+        if(word[i] == ch)
           secretWord[i] = ch;
     }
 }
