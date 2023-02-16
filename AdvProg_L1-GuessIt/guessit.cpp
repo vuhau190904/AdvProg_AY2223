49059d54506ee4d41e5dfa65439d7eb68a4e8602
vuhau190904 <vuhau190904@gmail.com>
2023-02-16 09:58:11 +0700

    update

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -74,7 +74,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if(isContinued == 'y' || isContinue == 'Y') result = true;
+    if(isContinued == 'y' || isContinued == 'Y') result = true;
     return result;
 }
 
